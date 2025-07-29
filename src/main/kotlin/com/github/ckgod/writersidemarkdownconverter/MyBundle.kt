0acf637fe2ac6f6006d270e63cf69fb5814e3f62
GitHub Action <action@github.com>
2025-07-29 23:20:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/ckgod/writersidemarkdownconverter/MyBundle.kt
+++ src/main/kotlin/com/github/ckgod/writersidemarkdownconverter/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ckgod.writersidemarkdownconverter
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ckgod/writersidemarkdownconverter/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/ckgod/writersidemarkdownconverter/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.ckgod.writersidemarkdownconverter.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.ckgod.writersidemarkdownconverter.MyBundle
+import com.github.ckgod.writersidemarkdownconverter.services.MyProjectService
 import javax.swing.JButton
 
 
