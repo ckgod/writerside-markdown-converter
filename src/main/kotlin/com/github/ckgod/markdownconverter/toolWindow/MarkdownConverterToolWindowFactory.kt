eb24c1e932a956b555f724499e1b7473cfc0234b
chako <chako@ebay.com>
2025-07-30 09:28:56 +0900

    fix: 기본 파일 네이밍 수정

--- src/main/kotlin/com/github/ckgod/markdownconverter/toolWindow/MarkdownConverterToolWindowFactory.kt
+++ src/main/kotlin/com/github/ckgod/markdownconverter/toolWindow/MarkdownConverterToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package com.github.ckgod.writersidemarkdownconverter.toolWindow
+package com.github.ckgod.markdownconverter.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,12 +8,19 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import com.github.ckgod.writersidemarkdownconverter.MyBundle
-import com.github.ckgod.writersidemarkdownconverter.services.MyProjectService
+import com.github.ckgod.markdownconverter.MarkdownConverterBundle
+import com.github.ckgod.markdownconverter.services.MarkdownConverterService
 import javax.swing.JButton
 
-
-class MyToolWindowFactory : ToolWindowFactory {
+/**
+ * 마크다운 변환기 플러그인의 메인 UI인 Tool Window를 생성합니다.
+ *
+ * 이 클래스는 `plugin.xml` 파일의 `<toolWindow>` 확장점에 등록되어야 하며,
+ * ID를 통해 인텔리제이 플랫폼이 인식하고 Tool Window를 초기화할 때 `createToolWindowContent` 메소드를 호출합니다.
+ *
+ * @see ToolWindowFactory
+ */
+class MarkdownConverterToolWindowFactory : ToolWindowFactory {
 
     init {
         thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
@@ -29,15 +36,15 @@ class MyToolWindowFactory : ToolWindowFactory {
 
     class MyToolWindow(toolWindow: ToolWindow) {
 
-        private val service = toolWindow.project.service<MyProjectService>()
+        private val service = toolWindow.project.service<MarkdownConverterService>()
 
         fun getContent() = JBPanel<JBPanel<*>>().apply {
-            val label = JBLabel(MyBundle.message("randomLabel", "?"))
+            val label = JBLabel(MarkdownConverterBundle.message("randomLabel", "?"))
 
             add(label)
-            add(JButton(MyBundle.message("shuffle")).apply {
+            add(JButton(MarkdownConverterBundle.message("shuffle")).apply {
                 addActionListener {
-                    label.text = MyBundle.message("randomLabel", service.getRandomNumber())
+                    label.text = MarkdownConverterBundle.message("randomLabel", service.getRandomNumber())
                 }
             })
         }
